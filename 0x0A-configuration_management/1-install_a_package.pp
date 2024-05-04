e446c14a2e08b254f4c9a74af67f07dcceef859a
Khaled Empeshy <empeshy20036969@gmail.com>
2024-05-04 14:45:34 +0300

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,9 @@
 # install a package
+package { 'Wekzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  before   => Package['flask'],
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
